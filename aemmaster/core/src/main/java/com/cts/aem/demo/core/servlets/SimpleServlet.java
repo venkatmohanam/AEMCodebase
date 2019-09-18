399244cf89396917db8cfc516e4b54f82298060b
Venkatesan <venkatesan.mohanam@cognizant.com>
2019-09-18 14:31:37 +0530

    VGJI-1 #comment updated author name in the SimpleServlet

--- aemmaster/core/src/main/java/com/cts/aem/demo/core/servlets/SimpleServlet.java
+++ aemmaster/core/src/main/java/com/cts/aem/demo/core/servlets/SimpleServlet.java
@@ -34,7 +34,7 @@ import java.io.IOException;
  * all resources of a specific Sling resource type. The
  * {@link SlingSafeMethodsServlet} shall be used for HTTP methods that are
  * idempotent. For write operations use the {@link SlingAllMethodsServlet}.
- Author:vmohanam updated on 9/18/2019
+ Author:venkatmohanam updated on 9/18/2019
  */
 @Component(service=Servlet.class,
            property={
